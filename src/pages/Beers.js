4c150cacb9296dae17e8ac6b5a3b15685553ddd3
Tatiana <taty07@gmail.com>
2022-11-18 17:25:22 +0100

    it7 done

--- src/pages/Beers.js
+++ src/pages/Beers.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 const Beers = ({ beers }) => {
   const [beersArr, setBeersArr] = useState([]);
+  const [filteredBeers, setFilteredBeers] = useState("");
 
   const fetchBeers = async (setter) => {
     try {
@@ -19,21 +20,45 @@ const Beers = ({ beers }) => {
     fetchBeers(setBeersArr);
   }, []);
 
+  const handleSearchInput = (event) => {
+    setFilteredBeers(event.target.value);
+  };
+
   return (
     <div>
       <Navbar />
-      {beersArr.map((beer) => {
-        return (
-          <div key={beer._id}>
-            <img src={beer.image_url} alt={beer.name} />
-            <Link to={`/beers/${beer._id}`}>
-              <h2>{beer.name}</h2>
-            </Link>
-            <p>{beer.tagline}</p>
-            <p>{beer.contributed_by}</p>
-          </div>
-        );
-      })}
+      <div>
+        <h2>Search food</h2>
+        <input
+          value={filteredBeers}
+          placeholder="Search for a beer"
+          tyye="text"
+          onChange={handleSearchInput}
+        />
+      </div>
+      <div>
+        {beersArr.length === 0 ? (
+          <h1>Oops! There is no more content to show!</h1>
+        ) : (
+          beersArr
+            .filter((beer) => {
+              const lowerFilter = filteredBeers.toLowerCase();
+              return beer.name.toLowerCase().includes(lowerFilter);
+            })
+            .map((beer) => {
+              return (
+                <div key={beer._id}>
+                  <img src={beer.image_url} alt={beer.name} />
+                  <Link to={`/beers/${beer._id}`}>
+                    <h2>{beer.name}</h2>
+                  </Link>
+                  <p>{beer.tagline}</p>
+                  <p>{beer.contributed_by}</p>
+                </div>
+              );
+            })
+        )}
+      </div>
     </div>
   );
 };
